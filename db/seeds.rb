9c6a8b45836fc928b45b8db22a161ab2e2e9a0af
Your Name <you@example.com>
2017-10-23 10:27:28 -0500

    creacion seed BD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create(title: "prueba titulo", body: "esto es una prueba", user_id: 1)
+Comment.create(body: "prueba comentario", user_id: 1, post_id: 1)
