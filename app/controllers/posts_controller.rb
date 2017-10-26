212d20bd3eb93689df085fb718fb5075deb67d33
Your Name <you@example.com>
2017-10-26 15:05:04 -0500

    Crea la funcionalidad de crear post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.order('created_at ASC')
 	end
+
+	def  new
+		@posts = Post.new	
+	end
+
+	def create
+		@posts = Post.new(post_params)
+		if @posts.save
+			redirect_to posts_path, :notice => "Posts guardado"
+		else
+			@errors = @posts.errors.full_messages
+      render :new	
+		end	
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title,:body).merge(user_id: current_user.id)
+		end
 end
