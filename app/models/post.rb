b7c132663f1ea7f283f261922bd220439046fa0a
Your Name <you@example.com>
2017-10-31 12:09:35 -0500

    ajuste de validacion del body

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 	validates :title, presence: true		
-	validates :body, presence: true		
+	validates :body, presence: true, length: { minimum: 250 }		
   belongs_to :user
   has_many :comment
 end
