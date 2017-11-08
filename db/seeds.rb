438e7138fc180ae54ccc329a0a77b921064c900a
Your Name <you@example.com>
2017-11-08 11:39:08 -0500

    seeds de prueba

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+User.create(name: 'Oscar', email: 'prueba1@prueba1.com', password: '123456')
+
+
 Post.destroy_all
+Post.create(title: "Titulo 1", body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: 1)
+
+
 Comment.destroy_all
-Post.create(title: "prueba titulo", body: "esto es una prueba", user_id: 1)
 Comment.create(body: "prueba comentario", user_id: 1, post_id: 1)
